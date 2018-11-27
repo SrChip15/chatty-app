c8ff8ad735ecccbca576b20cb20bf789b27b97e5
Sri Nath <sri.developer731@gmail.com>
2018-11-27 11:04:50 -0500

    Render React HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ChatBar from './ChatBar.jsx';
 
-class App extends Component {
-  render() {
+export default class App extends Component {
+  render () {
     return (
-      <h1>Hello React :)</h1>
+      <main className="messages">
+        <div className="message">
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+          </nav>
+          <div className="message">
+            <span className="message-username">Anonymous1</span>
+            <span className="message-content">I won't be impressed with technology until I can download food.</span>
+          </div>
+          <div className="message system">
+            Anonymous1 changed their name to nomnom.
+          </div>
+          <ChatBar />
+        </div>
+      </main>
     );
   }
 }
-export default App;

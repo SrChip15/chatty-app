97a45e94160785f4f2c617401d8266c0f1837a71
Sri Nath <sri.developer731@gmail.com>
2018-12-16 04:42:41 -0500

    Fix Message.jsx to display > 1 images

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,33 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-export default class Message extends Component {
-  render () {
-    const userColor = {
-      color: this.props.message.color,
-    }
+function GetImages(props) {
+	// Prop data element type validation
+	GetImages.propTypes = {
+		message: React.PropTypes.object,
+		user: React.PropTypes.object,
+	}
 
-    const msgImage = this.props.message.image;
-    const htmlImage = msgImage ? <img className="image" src={msgImage} /> : null;
+	// Look for image URL ONLY when the message DOES contain an image URL
+	if (props.message.image) {
+		const imageURLsAsArr = props.message.image.split(' ');
 
-    return (
-      <div
-        className="message"
-      >
-        <span
-          className="message-username"
-          style={userColor}
-        >
-          {this.props.message.username}
-        </span>
+		const imagesToDisplay = imageURLsAsArr.map((imageURL, idx) => {
+			return <img key={idx} className="image" src={imageURL}	/>
+		})
 
-        <p className="message-content">{this.props.message.content}</p>
-        {htmlImage}
-      </div>
-    );
-  }
+		return (
+			<div>
+				{imagesToDisplay}
+			</div>
+		);
+	}
+
+	return false;
 }
 
+export default class Message extends Component {
+	render() {
+		const userColor = {
+			color: this.props.message.color,
+		}
+
+		const htmlImage = GetImages(this.props);
+
+		return (
+			<div className = "message">
+				<span
+					className = "message-username"
+					style = {userColor}
+					>
+						{this.props.message.username}
+					</span>
+
+				<p
+					className = "message-content"
+					>
+						{this.props.message.content}
+				</p>
+				{htmlImage}
+				</div>
+		);
+	}
+}
+
+/** Prop data element type validation */
 Message.propTypes = {
-  message: React.PropTypes.object,
-  user: React.PropTypes.object,
+	message: React.PropTypes.object,
+	user: React.PropTypes.object,
 }
